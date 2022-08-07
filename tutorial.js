a4c5b6152020a48372fca0cdc2c6f8a9de39e2ef
KINGALVI <alvialmahamud@gmail.com>
2022-08-08 02:33:02 +0600

    added javascript

--- tutorial.js
+++ tutorial.js
@@ -612,10 +612,13 @@ lepyear(2022);
 
 function getsumofanarray (numbers){
   let sum=0;
+  for(let i =0; i <numbers.lenght;i++)
+{
   const index =i;
   const element=numbers[index];
   sum=sum +element;
   console.log(index,element,sum);
+}
 }
 const mynumbers=[12,65,45,78,32,45,91];
 getsumofanarray(mynumbers);
