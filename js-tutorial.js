f5e72d09c25ecdf0b2bf5e26546b971005096308
KINGALVI <alvialmahamud@gmail.com>
2022-08-14 22:50:22 +0600

    added javascript

--- js-tutorial.js
+++ js-tutorial.js
@@ -1,1 +1,18 @@
 // alert("Hello! I am an alert box!!");
+
+
+// ১ । javascript এ কোন HTML id কে পেতে চাইলে সে ক্ষেত্রে এই code টি লিখেতে হবে ।
+const ElementId =document.getElementById('al');
+  for(const al of ElementId){
+    console.log(al.innerText);
+  }
+
+// ২ । javascript এ কোন Element এর class কে পেতে চাইলে সে ক্ষেত্রে এই code টি লিখেতে হবে ।
+const Elementsclass =document.getElementsByClassName('al');
+  for(const alvi of Elementsclass){
+    console.log(alvi.innerText);
+  }
+
+// ৩ । javascript এ কোন tag , id , class যদি আমরা তাড়াতাড়ি পেতে চাই তবে সে ক্ষেত্রে quarryselector code ব্যবহার করতে হবে ।
+const quarryselector = document.querySelectorAll('.alvi');
+console.log(quarryselector);
