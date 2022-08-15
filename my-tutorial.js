d13e93b683eb23ff508a2bc56982b373322e041c
KINGALVI <alvialmahamud@gmail.com>
2022-08-15 10:31:45 +0600

    added javascript

--- my-tutorial.js
+++ my-tutorial.js
@@ -9,23 +9,38 @@
 // ৩ । javascript এ কোন tag , id , class যদি আমরা তাড়াতাড়ি পেতে চাই তবে সে ক্ষেত্রে quarryselector code ব্যবহার করতে হবে (quarryselector সাধারনত দুই ধরনের হয়ে থাকে ঃ ১। quarrySelector 2। querySelectorAll))।
 const quarryselector = document.querySelectorAll('.alvi');
 console.log(quarryselector);
-for(const alvi of quarryselector){
+for (const alvi of quarryselector) {
   console.log(alvi);
 }
 
 const seci = document.querySelectorAll('section');
-for(mi of seci){
-  seci.style.backgroundColor='red';
-  seci.style.color='yellow';
+for (mi of seci) {
+  seci.style.backgroundColor = 'red';
+  seci.style.color = 'yellow';
 }
 
 // ৪ । javascript এ কোন class বা id এর element পরিবতিন করতে চাইলে , সে ক্ষেত্রে এই code টী লিখেতে হবে ।
 const sec3 = document.getElementsByClassName('sec');
 const li = document.createElement('li')
-li.innerText='al mahamud'
+li.innerText = 'al mahamud'
 // sec3.appendChild(up)
 
 // ৩ । কোন button এ click করে যদি কোন কিছু পরিবতিন করেতে হয় , তাহলে সে ক্ষেত্রে নিচের এই code টি লিখেতে হবে । 
-function MakeRed(){
-  document.body.style.backgroundColor='red';
+
+// ১ম পদ্ধতি ।
+function MakeRed() {
+  document.body.style.backgroundColor = 'red';
+}
+
+// ২য় পদ্ধতি ।
+const MakeBlueButton = document.getElementById('MakeBlue');
+MakeBlueButton.onclick = MakeBlue;
+function MakeBlue() {
+  document.body.style.backgroundColor = 'blue';
+}
+
+// ৩য় পদ্ধতি ।
+const MakeGreenButton = document.getElementById('MakeGreen');
+MakeGreenButton.onclick = function MakeGreen() {
+  document.body.style.backgroundColor = 'green'
 }
