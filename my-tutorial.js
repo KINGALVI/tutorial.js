df8efae40419ef69a04e8f1a7eb1e3345115d56d
KINGALVI <alvialmahamud@gmail.com>
2022-08-16 10:19:20 +0600

    added javascript

--- my-tutorial.js
+++ my-tutorial.js
@@ -11,18 +11,18 @@ const quarryselector = document.querySelectorAll('.alvi');
 console.log(quarryselector);
 for (const alvi of quarryselector) {
   console.log(alvi);
-}
+};
 
 const seci = document.querySelectorAll('section');
 for (mi of seci) {
   seci.style.backgroundColor = 'red';
   seci.style.color = 'yellow';
-}
+};
 
 // ৪ । javascript এ কোন class বা id এর element পরিবতিন করতে চাইলে , সে ক্ষেত্রে এই code টী লিখেতে হবে ।
 const sec3 = document.getElementsByClassName('sec');
 const li = document.createElement('li')
-li.innerText = 'al mahamud'
+li.innerText = 'al mahamud';
 // sec3.appendChild(up)
 
 // ৩ । কোন button এ click করে যদি কোন কিছু পরিবতিন করেতে হয় , তাহলে সে ক্ষেত্রে নিচের এই code টি লিখেতে হবে । 
@@ -30,32 +30,32 @@ li.innerText = 'al mahamud'
 // ১ম পদ্ধতি ।
 function MakeRed() {
   document.body.style.backgroundColor = 'red';
-}
+};
 
 // ২য় পদ্ধতি ।
 const MakeBlueButton = document.getElementById('MakeBlue');
 MakeBlueButton.onclick = MakeBlue;
 function MakeBlue() {
   document.body.style.backgroundColor = 'blue';
-}
+};
 
 // ৩য় পদ্ধতি ।
 const MakeGreenButton = document.getElementById('MakeGreen');
 MakeGreenButton.onclick = function MakeGreen() {
   document.body.style.backgroundColor = 'green';
-}
+};
 
 // ৪থ পদ্ধতি । 
 const MakePinkButton = document.getElementById('MakePink')
 MakePinkButton.addEventListener('click',
   function MakePink() {
     document.body.style.backgroundColor = 'pink';
-  })
+  });
 
 // ৫ম পদ্ধতি ।
 document.getElementById('MakeOrange').addEventListener('click', function MakeOrange() {
   document.body.style.backgroundColor = 'goldrod';
-})
+});
 
 // কোন button এ ckick করে সেই button এর সাথে থাকা লেখা পরিবতিন করা যায় ।
 
@@ -63,17 +63,29 @@ document.getElementById('MakeOrange').addEventListener('click', function MakeOra
 function oneckicktext() {
   const textchange1 = document.getElementById('text')
   textchange1.innerText = 'my name is alvi'
-}
+};
 
 // ২য় পদ্ধতি ।
-document.getElementById('event-listener').addEventListener('click', function () {
+document.getElementById('event-listener1').addEventListener('click', function () {
   const textchange2 = document.getElementById('text');
   textchange2.innerText = 'i want to de a good wed devoleper'
-})
+});
 
-document.getElementById('btnupdate').addEventListener('click', function(){
-  const textupdate=document.getElementById('inputfield');
-  const updatetext =textupdate.value;
+//  status update বানানোতে ব্যবহার করা হয় । 
+document.getElementById('btnupdate').addEventListener('click', function () {
+  const textupdate = document.getElementById('inputfield');
+  const updatetext = textupdate.value;
   const p = document.getElementById('justupdatetext')
-  p.innerText=updatetext;
-   })
+  p.innerText = updatetext;
+});
+
+//   commentdox update বানানোতে ব্যবহার করা হয় ।
+document.getElementById('btn-post').addEventListener('click', function () {
+  const commenbox=document.getElementById('new-comment');
+  const newcomment=commenbox.value
+  const commentcontainer = document.getElementById('comment-container');
+  const p= document.createElement('p');
+  p.innerText=newcomment;
+  commentcontainer.appendChild(p);
+  commenbox.value='';
+})
